92c62258e3434acf3ee8ca5f90087dc86cbd7f88
Bao <phanbao010103@gmail.com>
2026-06-09 15:26:15 +0700

    Update TluLoginTest.java

--- src/test/java/com/phannhubao/logintest/TluLoginTest.java
+++ src/test/java/com/phannhubao/logintest/TluLoginTest.java
@@ -49,13 +49,13 @@ public class TluLoginTest {
     @Test
     public void testLoginAccount1() {
         System.out.println("Running login test 1...");
-        performLogin("2351067118", "077205009741");
+        performLogin("2351067086", "Bao112003@");
     }
 
     @Test
     public void testLoginAccount2() {
         System.out.println("Running login test 2...");
-        performLogin("2351067118", "077205009740");
+        performLogin("2351067086", "077205009740");
     }
 
     private void performLogin(String username, String password) {
